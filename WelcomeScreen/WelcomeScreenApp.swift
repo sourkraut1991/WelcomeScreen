8e7f9b350f86c47fd11ee1458ede0ad7b0802145
sourkraut1991 <69095925+sourkraut1991@users.noreply.github.com>
2022-12-31 16:59:31 +0300

    Initial Commit

---
+++ WelcomeScreen/WelcomeScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WelcomeScreenApp.swift
+//  WelcomeScreen
+//
+//  Created by ed on 12/31/22.
+//
+
+import SwiftUI
+
+@main
+struct WelcomeScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
